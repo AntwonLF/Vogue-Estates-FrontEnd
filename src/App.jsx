dec9bd56d925de432528650dea69503ef35e175d
wysmith00 <wysmith00@gmail.com>
2024-02-13 11:42:02 -0500

    nav complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom'
+import NavBar from './components/Navbar/Navbar'
+import Location from './pages/Location/Location'
+import Team from './pages/Team/Team'
+import Listing from './pages/Listing/Listing'
+import Landing from './pages/Landing/Landing'
 
-const App = () => {
+
+function App() {
+  const navigate = useNavigate();
+  // const [user, setUser] = useState(authService.getUser());
+
+  function handleSignout() {
+    authService.logout();
+    setUser(null);
+  }
+
   return (
-    <div>App</div>
-  )
+    <>
+      {/* <NavBar user={user} handleSignout={handleSignout} /> */}
+      <NavBar handleSignout={handleSignout} />
+      <Routes>
+        {/* <Route path="/" element={<Landing handleSignupOrLogin={handleSignupOrLogin} />} /> */}
+        <Route path='/' element={<Landing />} />
+        <Route path="/location" element={<Location />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/listing" element={
+          // <ProtectedRoute user={user}>
+            <Listing />
+          // </ProtectedRoute>
+        } />
+      </Routes>
+    </>
+  );
 }
-
 export default App
---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import SignupForm from '../../components/SignupForm/SignupForm';
+import LoginForm from '../../components/LoginForm/LoginForm';
+
+
+import './Landing.css'
+
+const Landing = ({ handleSignupOrLogin }) => {
+  const [message, setMessage] = useState('');
+  const [currentForm, setcurrentForm] = useState('login');
+
+  return (
+    <div className="landing-container">
+      <div className="brand-container">
+        <h1 className="brand-title">Style$tash</h1>
+        <p className="brand-subtitle">Your wardrobe on the Go</p>
+        <p className="brand-description">
+          Welcome to StyleStash, the innovative app created by Array of Attire that revolutionizes your shopping and wardrobe management experience! Have you ever found yourself in a store, wondering if you already own something similar to what's in your hand? StyleStash is your modern solution to this age-old dilemma.
+        </p>
+      </div>
+      {currentForm === 'login' ? (
+        <LoginForm
+          message={message}
+          setMessage={setMessage}
+          handleSignupOrLogin={handleSignupOrLogin}
+        />
+      ) : (
+        <SignupForm
+          message={message}
+          setMessage={setMessage}
+          handleSignupOrLogin={handleSignupOrLogin}
+        />
+      )}
+      <button onClick={() => setcurrentForm(currentForm === 'login' ? 'signup' : 'login')} className="toggle-button">
+        {currentForm === 'login' ? 'Sign Up' : 'Log In'}
+      </button>
+    </div>
+  );
+};
+
+export default Landing
+
+
+//needs to include signin and login 
+//button functionality in singup form and login form
