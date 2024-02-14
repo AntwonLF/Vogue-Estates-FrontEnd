d1cca33d144c86afaf271299a0e8c36ae0d306d0
wysmith00 <wysmith00@gmail.com>
2024-02-14 09:32:08 -0500

    testing signup

--- src/pages/Listing/Listing.jsx
+++ src/pages/Listing/Listing.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import { useNavigate } from 'react-router';
 
-import './listing.css'; 
+import './Listing.css'; 
 
 const Listing = () => {
     const navigate = useNavigate();
