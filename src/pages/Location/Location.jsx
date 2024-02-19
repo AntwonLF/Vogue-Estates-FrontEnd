8912acea5e748ac89d404ce4fa062a4b345e0664
cd <lesleyrotonto@gmail.com>
2024-02-19 14:02:49 -0500

    responsive landing and location page

--- src/pages/Location/Location.jsx
+++ src/pages/Location/Location.jsx
@@ -1,23 +1,17 @@
 import './Location.css';
-import miamiImage from '../../assets/Miami.png'
+
 
 const Location = () => {
-    
     return (
-      <div className="location-container">
-        <h1 className="location-title">What sets Vogue Estateś apart is the size of the homes, unique luxury tropical villas that are designed with the space, details, and amenities!</h1>
-        <div className="image-wrapper">
-          <iframe 
-          width="100%" 
-          height="600" 
-          frameBorder="0" 
-          scrolling="no" 
-          marginHeight="0" 
-          marginWidth="0" 
-          src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=3101%20NE%207th%20Ave,%20Miami,%20FL%2033137+(Vouge%20Estates)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
-          <a href="https://www.gps.ie/">gps tracker sport</a>
-        </iframe>
+      <div>
+        <div className="container__inner"></div>
+      <div style={{ borderTop: "2px solid #000000 ", marginLeft: 20, marginRight: 20 }}></div>
+        <div className="location-container">
+          <h1 className="location-title">While you visit our office take a stroll and get to know your new neighborhood</h1>
+          <div className="image-wrapper">
+          <iframe  width="520" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=3101%20NE%207th%20Ave,%20Miami,%20FL%2033137+(Vogue%20Estates)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps vehicle tracker</a></iframe>
 
+          </div>
         </div>
       </div>
     );
